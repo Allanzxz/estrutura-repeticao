3cb5ad981fc7764e0999bdc92a92ab8d72949317
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-03 10:42:41 -0400

    finalizada questao10

---
+++ questao10.js
@@ -0,0 +1,9 @@
+/*
+10. Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10).  
+ Estrutura de repetição do...while: 
+*/
+resposta: let i = 1;
+while (i <= 10) {
+    console.log("5 x " + i + " = " + (5 * i));
+    i++;
+}
